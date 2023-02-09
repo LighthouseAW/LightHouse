9e61db07927497e3677cd562d6c4a246bc24e162
JonnyNice <ianholsteen@gmail.com>
2023-02-09 12:24:18 -0800

    fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,25 @@
 Rails.application.routes.draw do
+  namespace :api do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :instrumentals, only: [:index, :show]
-  resources :audio_files, only: [:index, :show]
-  resources :users
-  resources :carts
-  resources :orders, only: [:index, :create, :show, :update, :destroy]
-  resources :carts, only: [:create, :show] do
-    resources :orders, only: :create
-  end
-  resources :leases
+    resources :instrumentals, only: [:index, :show]
+    resources :audio_files, only: [:index, :show]
+    resources :users
+    resources :carts
+    resources :orders, only: [:index, :create, :show, :update, :destroy]
+    resources :carts, only: [:create, :show] do
+      resources :orders, only: :create
+    end
+    resources :leases
 
 
 
 
   # Defines the root path route ("/")
   # root "articles#index"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
-  get "/me", to: "users#show"
-  post "/signup", to: "users#create"
-
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+    get "/me", to: "users#show"
+    post "/signup", to: "users#create"
+  end
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
