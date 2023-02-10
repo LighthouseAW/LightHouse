7969a48ed792204ffb911522d2748cb9a57d2e43
JonnyNice <ianholsteen@gmail.com>
2023-02-10 13:55:52 -0800

    fixing9

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::UsersController < ApplicationController
-    skip_before_action :authorize, only: [:create, :show, :index]
+    skip_before_action :authorize, only: [:create, :guest, :show, :index]
     before_action :find_user, only: [:update, :destroy]
     before_action :guest, only: [:show]
 
