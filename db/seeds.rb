f05cf3e3c5e215b1365bf0c9c09334c52d1f402a
JonnyNice <ianholsteen@gmail.com>
2023-02-10 15:56:37 -0800

    fixing19

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 Cart.destroy_all
 User.destroy_all
 Order.destroy_all
-# AudioFile.destroy_all
+AudioFile.destroy_all
 Instrumental.destroy_all
 Genre.destroy_all
 Lease.destroy_all
@@ -19,12 +19,12 @@ l2 = Lease.create(contract_info: 'mp3 lease, unlimited use', price: 40)
 l3 = Lease.create(contract_info: 'mp3 lease, unlimited use', price: 40)
 
 i1mp3 = AudioFile.create(instrumental_id: i1.id, lease: l1)
+i1mp3.file.attach(io: File.open('./public/Vile x train x flemps.mp3'), filename: 'Vile x train x flemps.mp3', content_type: 'audio/mp3')
+
 i2mp3 = AudioFile.create(instrumental_id: i2.id, lease: l2)
-i3mp3 = AudioFile.create(instrumental_id: i3.id, lease: l3)
-
-# i1mp3.file.attach(io: File.open('./public/Vile x train x flemps.mp3'), filename: 'Vile x train x flemps.mp3', content_type: 'audio/mp3')
 i2mp3.file.attach(io: File.open('./public/ritual.mp3'), filename: 'Again.mp3', content_type: 'audio/mp3')
-# i3mp3.file.attach(io: File.open('./public/audio/khxncollab.mp3'), filename: 'khxncollab.mp3', content_type: 'audio/mp3')
 
+i3mp3 = AudioFile.create(instrumental_id: i3.id, lease: l3)
+i3mp3.file.attach(io: File.open('./public/audio/khxncollab.mp3'), filename: 'khxncollab.mp3', content_type: 'audio/mp3')
 
 puts "seeded ✅:"
