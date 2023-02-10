2a88dad9217f3335c046c2186da85ecb7979a6d4
JonnyNice <ianholsteen@gmail.com>
2023-02-10 12:35:17 -0800

    fixing3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Cart.destroy_all
 User.destroy_all
-Cart.destroay_all
 Order.destroy_all
 AudioFile.destroy_all
 Instrumental.destroy_all
