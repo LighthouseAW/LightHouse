b405590db9b2a83a6f6b698816f7162d29c3af3f
JonnyNice <ianholsteen@gmail.com>
2023-02-09 17:12:16 -0800

    starting to understand3

--- client/next.config.js
+++ client/next.config.js
@@ -6,16 +6,4 @@ const nextConfig = {
     unoptimized: true,
   },
 }
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:3000/:path*'
-      },
-    ]
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+module.exports = nextConfig
