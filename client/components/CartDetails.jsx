9c288c43002b8cd116e99dd0448ae4d979b7d974
JonnyNice <ianholsteen@gmail.com>
2023-02-11 11:36:13 -0800

    new one

--- client/components/CartDetails.jsx
+++ client/components/CartDetails.jsx
@@ -73,7 +73,7 @@ export default function CartDetails () {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                cartId: cart.id
+                cart_id: cart.id
             })
             })
             .then(response => {
