98144a57866964af5f80cccce8a8348a033d4b89
JonnyNice <ianholsteen@gmail.com>
2023-02-15 14:47:27 -0800

    cccharge

--- app/controllers/api/charges_controller.rb
+++ app/controllers/api/charges_controller.rb
@@ -3,29 +3,28 @@ require 'dotenv'
 
 Dotenv.load
 
-class Api::ChargesController < ApplicationController
-    skip_before_action :authorize
+        # Set the API key for Strip
 
-    def create
-        # Retrieve the price from the request parameters
-        price = params[:price]
+    class Api::ChargesController < ApplicationController
+        skip_before_action :authorize
+        def create
+            amount = params[:price]
+            token = params[:token]
 
-        # Set the API key for Stripe
-        Stripe.api_key = "sk_test_51MYwEsDhAuw7r76W3hIw2fQnVVJixwEojclZH7jsv7s0bvV091uNaJo0ejjmgiykMjP3tzBtgxkGV9Vig4rzxjrU00a4mTUS7r"
+            Stripe.api_key = "sk_test_51MYwEsDhAuw7r76W3hIw2fQnVVJixwEojclZH7jsv7s0bvV091uNaJo0ejjmgiykMjP3tzBtgxkGV9Vig4rzxjrU00a4mTUS7r"
+            customer = Stripe::Customer.create(source: token)
 
-        # Create a charge for the given price
-        charge = Stripe::Charge.create({
-        amount: price,
-        currency: 'usd',
-        source: params[:token],
-        description: 'Example charge',
-        })
-
-        # Return a success response to the client
-        render json: { status: 204 }
-    rescue Stripe::CardError => e
-        # Return an error response to the client if the charge was declined
-        render json: { error: e.message }, status: :unprocessable_entity
-    end
-end
-
+            charge = Stripe::Charge.create(
+                amount: amount,
+                currency: 'usd',
+                customer: customer.id
+            )
+            if charge.paid
+                render json: { message: 'Payment processed successfully' }, status: :ok
+            else
+                render json: { error: 'Payment processing failed' }, status: :unprocessable_entity
+            end
+            rescue Stripe::CardError => e
+                render json: { error: e.message }, status: :unprocessable_entity
+            end
+        end
