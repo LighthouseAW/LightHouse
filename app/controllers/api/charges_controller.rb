cf9c7aec03b6e76ff83f95477b04ab8879b318ed
JonnyNice <ianholsteen@gmail.com>
2023-02-15 11:59:14 -0800

    charge changes

--- app/controllers/api/charges_controller.rb
+++ app/controllers/api/charges_controller.rb
@@ -6,14 +6,14 @@
         skip_before_action :authorize
 
         def create
-            Stripe.api_key = "pk_test_51MYwEsDhAuw7r76Wi0QsPuD8nJRJ4e0xn07ItYgI4CsZGNHneFLuQoXssdoroMlJ6atdCgjZT8c2SZ78Vxic0LRh00lR6iza07"
+            Stripe.api_key = "sk_test_51MYwEsDhAuw7r76W3hIw2fQnVVJixwEojclZH7jsv7s0bvV091uNaJo0ejjmgiykMjP3tzBtgxkGV9Vig4rzxjrU00a4mTUS7r"
             customer = Stripe::Customer.create
             ephemeralKey = Stripe::EphemeralKey.create({
                 customer: customer['id'],
             }, {stripe_version: '2022-11-15'})
             paymentIntent = Stripe::PaymentIntent.create({
-                amount: 1099,
-                currency: 'eur',
+                amount: charge,
+                currency: 'usd',
                 customer: customer['id'],
                 automatic_payment_methods: {
                 enabled: true,
