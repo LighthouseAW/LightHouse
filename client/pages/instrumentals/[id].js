5493385e2433fa4c3b650cd9619a333f3d687fca
JonnyNice <ianholsteen@gmail.com>
2023-02-16 08:34:49 -0800

    eslint

--- client/pages/instrumentals/[id].js
+++ client/pages/instrumentals/[id].js
@@ -75,9 +75,9 @@ export default function Instrumental () {
                             }}
                         />
                 </div>
-                <h1>{instrumental.audio_files.map((audio_file) => {
+                <h1>{instrumental.audio_files.map((audio_file, index) => {
                     return (
-                        <div>
+                        <div key={index} >
                             <h1>{audio_file.lease.contract_info}</h1>
                             <h2>{audio_file.lease.price}</h2>
                         </div>
