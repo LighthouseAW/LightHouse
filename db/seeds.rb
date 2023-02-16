f48ee179265310f2efe06580b4b773479123975e
JonnyNice <ianholsteen@gmail.com>
2023-02-15 23:15:09 -0800

    seed change

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,23 @@
+Order.destroy_all
+Cart.destroy_all
+Purchase.destroy_all
+User.destroy_all
 
-# Order.destroy_all
-# Cart.destroy_all
-# Purchase.destroy_all
-# User.destroy_all
+AudioFile.destroy_all
+Instrumental.destroy_all
+Genre.destroy_all
+Lease.destroy_all
 
-# AudioFile.destroy_all
-# Instrumental.destroy_all
-# Genre.destroy_all
-# Lease.destroy_all
+g1 = Genre.create(name: "Drill")
 
+instrumentals_data = [  {    title: "Vile",    picture: "",    info: "about this track",    genre: Genre.first,    lease_info: 'mp3 lease, unlimited use',    price: 40,    contract_file_path: './public/contracts/*mp3 license.pdf',    audio_file_path: './public/audio/Vile x train x flemps.mp3'  },  {    title: "Ritual",    picture: "",    info: "about this track",    genre: Genre.first,    lease_info: 'mp3 lease, unlimited use',    price: 40,    contract_file_path: './public/contracts/*mp3 license.pdf',    audio_file_path: './public/audio/Ritual.mp3'  },  {    title: "Idols",    picture: "",    info: "about this track",    genre: Genre.first,    lease_info: 'mp3 lease, unlimited use',    price: 40,    contract_file_path: './public/contracts/*mp3 license.pdf',    audio_file_path: './public/audio/khxncollab.mp3'  }]
 
-
-# g1 = Genre.create(name: "Drill")
-
-
-# i1 = Instrumental.create(title: "Vile", picture: "", info: "about this track", genre: Genre.first)
-# l1 = Lease.create(contract_info: 'mp3 lease, unlimited use', price: 40)
-# l1.contract.attach(io: File.open('./public/contracts/*mp3 license.pdf'), filename: 'mp3 license agreement', content_type: "application/pdf")
-# i1mp3 = AudioFile.create(instrumental_id: i1.id, lease: l1)
-# i1mp3.file.attach(io: File.open('./public/audio/Vile x train x flemps.mp3'), filename: 'Vile x train x flemps.mp3', content_type: 'audio/mp3')
-
-
-# i2 = Instrumental.create(title: "Ritual", picture: "", info: "about this track", genre: Genre.first)
-# l2 = Lease.create(contract_info: 'mp3 lease, unlimited use', price: 40)
-# l2.contract.attach(io: File.open('./public/contracts/*mp3 license.pdf'), filename: 'mp3 license agreement', content_type: "application/pdf")
-# i2mp3 = AudioFile.create(instrumental_id: i2.id, lease: l2)
-# i2mp3.file.attach(io: File.open('./public/audio/Ritual.mp3'), filename: 'Again.mp3', content_type: 'audio/mp3')
-
-
-i3 = Instrumental.create(title: "Idols", picture: "", info: "about this track", genre: Genre.first)
-l3 = Lease.create(contract_info: 'mp3 lease, unlimited use', price: 40)
-l3.contract.attach(io: File.open('./public/contracts/*mp3 license.pdf'), filename: 'mp3 license agreement', content_type: "application/pdf")
-i3mp3 = AudioFile.create(instrumental_id: i3.id, lease: l3)
-i3mp3.file.attach(io: File.open('./public/audio/khxncollab.mp3'), filename: 'khxncollab.mp3', content_type: 'audio/mp3')
-
+instrumentals_data.each do |data|
+    instrumental = Instrumental.create(data.slice(:title, :picture, :info, :genre))
+    lease = Lease.create(contract_info: data[:lease_info], price: data[:price])
+    lease.contract.attach(io: File.open(data[:contract_file_path]), filename: 'mp3 license agreement', content_type: "application/pdf")
+    audio_file = AudioFile.create(instrumental: instrumental, lease: lease)
+    audio_file.file.attach(io: File.open(data[:audio_file_path]), filename: data[:title], content_type: 'audio/mp3')
+end
 
 puts "seeded ✅:"
