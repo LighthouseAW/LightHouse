6090bf504233e379f24f26bab3d39a7e15b489db
JonnyNice <ianholsteen@gmail.com>
2023-02-16 13:40:36 -0800

    stuff

--- client/components/CartItemCard.jsx
+++ client/components/CartItemCard.jsx
@@ -50,7 +50,25 @@ export default function CartItemCard({cartItem, handleDeleteData, updateCartItem
                     <div className='pt-5 pb-5'>
                         <p className='text-neutral-500'>{cartItem.lease.contract_info}</p>
                     </div>
-                    <button className="text-sm font-medium underline" value={cartItem.id} onClick={handleDelete}>Delete</button>
+                    <div className="flex">
+                    <div className="relative inline-block w-48">
+                        <select className="w-full py-2 pl-3 pr-8 text-base border-gray-300 focus:outline-none focus:ring focus:ring-blue-200 focus:border-blue-500" onChange={(e) => handleSwitch(e.target.value)}>
+                            {cartItem.instrumental.audio_files.map((audio_file, i) => {
+                                return audio_file.lease ? (
+                                    <option value={audio_file.lease.id} key={i} selected={audio_file.lease.id === cartItem.lease.id}>
+                                        {audio_file.lease.contract_info} - ${audio_file.lease.price}
+                                    </option>
+                                ) : null
+                            })}
+                        </select>
+                        <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+                            <svg className="w-4 h-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M6.293 6.293a1 1 0 011.414 0L10 8.586l2.293-2.293a1 1 0 011.414 1.414l-2.293 2.293 2.293 2.293a1 1 0 01-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 01-1.414-1.414l2.293-2.293-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
+                            </svg>
+                        </div>
+                    </div>
+                    <button className="text-sm font-medium underline ml-2" onClick={handleDelete}>Delete</button>
+                </div>
                 </div>
             </div>
         </div>
--- client/pages/index.js
+++ client/pages/index.js
@@ -20,14 +20,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Hero heading="Jonny Nice" message="Producer, Pianist, Composer" />
-      <div className='mx-48'>
+      {/* <div className='mx-48'>
         <div className='flex items-center justify-center m-auto w-4/6'>
           <p className='mt-10 text-center text-4xl font-bold mb-20'>
             Jonny Nice Productions
           </p>
         </div>
         <Carousel />
-      </div>
+      </div> */}
     </>
   )
 }
