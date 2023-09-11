57924a6221af9b59063e1a5919e02497651a75fe
JonnyNice <ianholsteen@gmail.com>
2023-09-11 11:53:21 -0700

    changed client name

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,9 +6,9 @@ bundle install
 # bundle exec rake assets:precompile
 # bundle exec rake assets:clean
 
-npm i --prefix src && npm run build --prefix client
+npm i --prefix client && npm run build --prefix client
 
-cp -a src/out/. public/
+cp -a client/out/. public/
 
 
 bundle exec rake db:migrate
