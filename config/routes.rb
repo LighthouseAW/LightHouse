6cc7377175e818a57232821a081256f6c9d78ff7
JonnyNice <ianholsteen@gmail.com>
2023-09-11 17:21:12 -0700

    dig

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :employees
-  resources :prayer_requests
-  resources :blog_photos
-  resources :blogposts
-  resources :forms
-  resources :project_photos
-  resources :projects
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
-    post "/login", to: "sessions#create"
-    delete "/logout", to: "sessions#destroy"
-    get "/me", to: "users#show"
-    post "/signup", to: "users#create"
+  namespace :api do
+    resources :users
+    resources :employees
+    resources :prayer_requests
+    resources :blog_photos
+    resources :blogposts
+    resources :forms
+    resources :project_photos
+    resources :projects
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    
+      post "/login", to: "sessions#create"
+      delete "/logout", to: "sessions#destroy"
+      get "/me", to: "users#show"
+      post "/signup", to: "users#create"
 
+  end
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
