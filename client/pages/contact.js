c447a934eb31be8f20119b0a0ff4cf2291f87f0f
JonnyNice <ianholsteen@gmail.com>
2023-09-18 12:18:22 -0700

    added contact

---
+++ client/pages/contact.js
@@ -0,0 +1,16 @@
+import HomeLayout from '../components/HomeLayout';
+import Contacto from '../components/Contact';
+
+export default function Contact() {
+    return (
+        <HomeLayout>
+            <div className='h-screen bg-about bg-cover bg-no-repeat'>
+                <div className="absolute inset-0 bg-gradient-to-b from-transparent to-white"></div>
+                    <div className="flex flex-col items-center justify-center pt-20 text-white"></div>
+                        <Contacto />
+                    </div>
+                <div className='grid mb-20 items-center justify-center'>
+            </div>
+        </HomeLayout>
+    )
+}
