4ecfb262281a474d845ce1a06f679f61e2455b3f
JonnyNice <ianholsteen@gmail.com>
2023-09-18 12:34:59 -0700

    picture for about

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
         'car2': "url('../public/carousel/Carousel4.jpeg')",
         'car3': "url('../public/carousel/Carousel2.jpeg')",
         'car4': "url('../public/carousel/Carousel5.jpeg')",
+        'staffPic': "url('../public/images/staff.jpeg')",
         'rug': "url('../public/images/Rug3.png')",
         'rug': "url('../public/images/Rug3.png')",
         'rug': "url('../public/images/Rug3.png')"
