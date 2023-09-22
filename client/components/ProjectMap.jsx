28f648d1098b6e140e5719fafb1423eb60c2ff77
JonnyNice <ianholsteen@gmail.com>
2023-09-22 12:05:57 -0700

    lint and image fix

--- client/components/ProjectMap.jsx
+++ client/components/ProjectMap.jsx
@@ -10,7 +10,7 @@ export default function ProjectMap({ projects }) {
                 <div key={index} className="bg-blue-300/50 rounded-lg shadow-md p-4 hover:shadow-lg transition duration-300 flex flex-col justify-center items-center z-20">
                     <Link href={`/projects/${project.id}`} className="font-semibold text-black text-lg mb-2 text-center">{project.name} →</Link>
                         <div className="image-container">
-                            <img src={photoUrl} alt="Project Photo" layout="responsive" width={300} height={300} />
+                            <Image src={photoUrl} alt="Project Photo" layout="responsive" width={300} height={300} />
                         </div>
                     <div className="text-black text-center mt-2">{project.subtitle}</div>
                 </div>
