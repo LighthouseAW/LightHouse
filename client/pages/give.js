59f54eda04a4725df6eec60d054b762c204a3d88
JonnyNice <ianholsteen@gmail.com>
2023-10-02 13:35:43 -0700

    give  page

--- client/pages/give.js
+++ client/pages/give.js
@@ -3,11 +3,10 @@ import HomeLayout from '../components/HomeLayout';
 import Link from "next/link"
 import DonatingSection from "../components/DonatingSection"
 import GiftInfo from "../components/GiftInfo"
+import Contact from "../components/Contact"
 
 export default function Give() {
     const [scriptLoaded, setScriptLoaded] = useState(false);
-
-    const title = "MAKING GOD'S LOVE VISIBLE TO EVERYONE IN THE MIDDLE EAST AND NORTH AFRICA"
 
     useEffect(() => {
         if (!scriptLoaded) {
@@ -27,6 +26,8 @@ export default function Give() {
         }
     }, [scriptLoaded]);
 
+    const title = "MAKING GOD'S LOVE VISIBLE TO EVERYONE IN THE MIDDLE EAST AND NORTH AFRICA"
+
     return (
         <HomeLayout>
             <div className='h-screen bg-about bg-cover bg-no-repeat '>
@@ -34,38 +35,31 @@ export default function Give() {
                 <div className="absolute inset-0 bg-gradient-to-b from-transparent to-color"></div>
                 <div className="flex items-center justify-center text-white relative z-20">
                     <div className="flex w-full">
-                        {/* <div className="w-3/5"> */}
                             <div className='kindful-embed-wrapper pt-24' id='kindful-donate-form-c685c50a-25c6-48be-bf8e-1a34b337a64c'></div>
-                        {/* </div> */}
                         <div className="w-2/5  pt-28 text-black z-20">
                             <GiftInfo />
+                            <DonatingSection />
                         </div>
                     </div>
                     {/* <div className="absolute bg-white w-full h-[700px]"></div> */}
+
                     {/* <div className="absolute bottom-96 bg-white w-full h-96"></div> */}
                 </div>
             </div>
-            <div className='relative bg-rug h-96 bg-no-repeat bg-cover bg-center '>
+            <div className='relative bg-rug h-96 mt-40 bg-no-repeat bg-cover bg-center '>
                 <div className="absolute inset-0 bg-gradient-to-b from-transparent to-color"></div>
-                <div className="absolute inset-0 bg-gradient-to-b from-color/75 to-transparent"></div>
-                    <div className='h-96'>
-                    </div>
+                {/* <div className="absolute inset-0 bg-gradient-to-b from-color/75 to-transparent"></div> */}
+
                 </div>
-            <div className="bg-color py-10 ">
-                <div className='z-20 relative'>
-                    <div className="flex items-center">
-                        <div className="absolute bg-white h-80" ></div>
-                    </div>
-                </div>
-            </div>
-            <div className='pb-20 pt-10 items-center px-20'>
-                <div className="flex w-full">
+            <div className='pb-6 items-center px-20'>
+                {/* <div className="flex w-full">
                         <div className="w-3/5 z-20 relative">
                         </div>
                         <div className="w-2/5 text-black z-20">
                         <DonatingSection />
                         </div>
-                    </div>
+                    </div> */}
+                    <Contact />
             </div>
         </HomeLayout>
     )
