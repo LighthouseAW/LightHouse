75291659394d647ddd119425e06ae2b67ce09e6b
JonnyNice <ianholsteen@gmail.com>
2023-10-08 22:26:22 -0700

    mission

--- client/pages/mission/index.js
+++ client/pages/mission/index.js
@@ -1,4 +1,5 @@
 import HomeLayout from '../../components/HomeLayout';
+import MissionNav from '../../components/NavBars/MissionNav'
 import Link from 'next/link'
 import Image from 'next/image'
 import { useActiveLink } from '../../context/ActiveLinkContext'
@@ -19,9 +20,9 @@ export default function Mission() {
         <HomeLayout>
             <div className='h-full bg-white bg-cover bg-no-repeat flex items-center justify-center '>
                 <div className="absolute inset-0 z-20 bg-gradient-to-b from-slate-300 to-transparent"></div>
-                <div className="px-44 relative z-20 pt-48">
+                <div className="px-32 relative z-20 pt-48">
                         <p className={`z-20 pb-4 text-black text-5xl font-bold`}>{missionTitle}</p>
-                        <div className='flex  space-x-16'>
+                        <div className='flex  space-x-14'>
                             <div className="w-1/2 flex  relative z-20">
                                 <p className={`z-20 text-black text-2xl`}>{missionWords}</p>
                             </div>
@@ -61,7 +62,7 @@ export default function Mission() {
             </div>
         </div>
             <div className='flex items-center justify-center  bg-color '>
-                <div className="px-44 relative z-20 pb-20 pt-20 ">
+                <div className="px-32 relative z-20 pb-20 pt-20 ">
                     <p className={`z-20 pb-4 text-black text-4xl text-right font-bold pl-96 `}>{visionTitle2}</p>
                         <div className='flex space-x-16'>
                         <div className="w-1/2 relative z-20 flex items-center justify-center pb-20">
@@ -77,7 +78,23 @@ export default function Mission() {
                             </div>
                         </div>
                     </div>
-            </div>
+                {/* <div className="flex items-center justify-center py-32 z-20">
+                    <div className="w-5/6 z-20 flex items-center justify-center space-x-6  ">
+                        <div className="w-1/2  relative z-20">
+                            <div>
+                                <div className="relative">
+                                    <Image alt="picture" src="/images/vision.jpeg" width="750" height="750" className="z-20 relative" />
+                                    <svg className="z-10 absolute top-0 left-0 -mt-11 -ml-24 hidden lg:block" width="504" height="384" fill="none" viewBox="0 0 404 384" aria-hidden="true"><defs><pattern id="de316486-4a29-4312-bdfc-fbce2132a2c1" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse"><rect x="0" y="0" width="2" height="2" className="text-mustard" fill="black"></rect></pattern></defs><rect width="404" height="384" fill="url(#de316486-4a29-4312-bdfc-fbce2132a2c1)"></rect><rect width="404" height="384" fill="url(#de316486-4a29-4312-bdfc-fbce2132a2c1)"></rect></svg>
+                                </div>
+                            </div>
+                        </div>
+                    <div className="w-1/2  relative z-20">
+                        <p className={`z-20 text-black text-6xl font-bold`}>Our Vision</p>
+                        <p className={`z-20 text-black py-5 text-3xl`}>We envision a future where the gospel of Jesus Christ is advancing among all peoples of the Arab World, where believers are bold and empowered to reach out to their neighbors, where the Church is healthy, growing and united in love, and where believers from Muslim backgrounds are living with dignity and thriving in their own cultures while contributing to its flourishing.</p>
+                    </div>
+                        </div>
+                </div> */}
+        </div>
         </HomeLayout>
     )
 }
