b1c3b57f23d87d1eb0b26897ccfd4359936bedd5
JonnyNice <ianholsteen@gmail.com>
2023-10-12 16:12:46 -0700

    seed fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -58,7 +58,7 @@ pLighthouseCin = Project.create(name: 'Lighthouse Cinema',
 
 pBeHelp = Project.create(name: 'BeHelp',
     subtitle: 'Lighthouse Arab World ministers to the poor, the needy and the outcast in the name of Jesus Christ.', 
-    blurb: "Breaking down walls of hate and building bridges through Cinema and Documentaries", 
+    blurb: "", 
     mission_statement: "Just as Jesus ministered to and cared for those in need, Lighthouse Arab World ministers to the poor, the needy, and the outcast in the name of Christ.", 
     about: "The Arab World is known for natural and man-made disasters, violence, blood shed and especially persecution of Christians. Our of our areas of focus and guiding principles is to opportunistically provide food, shelter, medicine, mental health care and spiritual encouragement for those most impacted by these events and circumstances.",
     website: "",
