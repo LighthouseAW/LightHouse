c1661043a1543aa5c84eab853a95f5cc05ea5b97
JonnyNice <ianholsteen@gmail.com>
2024-01-26 10:44:46 -0800

    timestamp for photourl;

--- client/components/Projects/ProjectMap.jsx
+++ client/components/Projects/ProjectMap.jsx
@@ -7,27 +7,24 @@ export default function ProjectMap({ projects }) {
             {projects.map((project, index) => {
                 const firstPhoto = project.project_photos[0];
                 if (firstPhoto) {
-                  const photoToken = firstPhoto.photo_token;
-                  const photoUrl = `https://lighthouseaw.onrender.com${firstPhoto.photo_url}?token=${photoToken}`;
-                  
-                  // Log the generated URL for debugging
-                  console.log("Generated Photo URL:", photoUrl);
-        
-                  return (
+                    const photoToken = firstPhoto.photo_token;
+                    const timestamp = new Date().getTime();
+                    const photoUrl = `https://lighthouseaw.onrender.com${firstPhoto.photo_url}?token=${photoToken}&timestamp=${timestamp}`;
+
+                    console.log("Generated Photo URL:", photoUrl);
+                return (
                     <div key={index} className="bg-slate-300/70 rounded-lg shadow-md p-4 hover:shadow-lg transition duration-300 flex flex-col justify-center items-center z-20">
-                      <Link href={`/projects/${project.slug}`} className="underline font-semibold text-black text-lg mb-2 text-center">{project.name}</Link>
-                      <div className="image-container">
-                        <Image src={photoUrl} alt="Project Photo" layout="responsive" width={300} height={300} />
-                      </div>
-                      <div className="text-black text-center mt-2">{project.subtitle}</div>
-                      <Link href={`/projects/${project.slug}`} className="font-semibold text-black text-lg text-center">Learn More →</Link>
+                        <Link href={`/projects/${project.slug}`} className="underline font-semibold text-black text-lg mb-2 text-center">{project.name}</Link>
+                            <div className="image-container">
+                                <Image src={photoUrl} alt="Project Photo" layout="responsive" width={300} height={300} />
+                            </div>
+                            <div className="text-black text-center mt-2">{project.subtitle}</div>
+                        <Link href={`/projects/${project.slug}`} className="font-semibold text-black text-lg text-center">Learn More →</Link>
                     </div>
-                  );
-                }
-        
-                // If project_photos is empty, return null or another placeholder
+                );
+            }
                 return null;
-              })}
-            </div>
+            })}
+        </div>
     )
 }
