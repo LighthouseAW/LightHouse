3de556e9d459e7cc59d084f18f5c17dbfbb4c4e1
LighthouseAW <149816571+LighthouseAW@users.noreply.github.com>
2026-05-20 23:00:52 -0400

    Update give.js

--- client/pages/give.js
+++ client/pages/give.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import Script from "next/script";
 import HomeLayout from '../components/Index/HomeLayout';
-import Link from "next/link"
 import DonatingSection from "../components/Give/DonatingSection"
 import GiftInfo from "../components/Give/GiftInfo"
 import Contact from "../components/Contact"
@@ -14,11 +13,8 @@ export default function Give() {
             const userAgent = window.navigator.userAgent;
             setIsMobile(/iPhone|iPad|iPod|Android/i.test(userAgent));
         };
-
         checkIsMobile();
-
         window.addEventListener("resize", checkIsMobile);
-
         return () => {
             window.removeEventListener("resize", checkIsMobile);
         };
@@ -28,41 +24,47 @@ export default function Give() {
 
     return (
         <HomeLayout>
-            
-            <div className='h-screen bg-about bg-cover bg-no-repeat '>
-                <div className={`${isMobile ? " px-2 text-2xl" : "px-48 text-3xl"} static  pt-36 z-20 text-white `}><h2>{title}</h2></div>
-                <div className={`absolute inset-0 ${isMobile ? "" : "bg-gradient-to-b from-transparent to-color"}`}></div>
-                <div className="flex-col text-white relative z-20">
-    <script src="https://s3-us-west-2.amazonaws.com/bloomerang-public-cdn/lighthousearabworld/.widget-js/113664.js" type="text/javascript"></script>
+            <div className='h-screen bg-about bg-cover bg-no-repeat'>
+                <div className={`${isMobile ? "px-2 text-2xl" : "px-48 text-3xl"} static pt-36 z-20 text-white`}>
+                    <h2>{title}</h2>
                 </div>
-                    { isMobile ? 
-                    <div className = "flex flex-col pt-8">
-                        {/* <div id='bloomerang-form-113664'></div> */}
-                        <div>
-                        <GiftInfo />
-                        </div>
-                        <div className="pb-8">
-                        <DonatingSection />
-                        </div>
-                    </div> : 
-                <div className="flex-col">
-                    <div className="flex w-full px-12">
-                    <div id='bloomerang-form-113664' className='pt-4 absolute left-40 top-0'></div>
-                            <div className="w-1/2 pt-4 text-black z-20">
-                            <GiftInfo />
-                        </div>
-                            <div className="w-1/2 pt-28">
-                            <DonatingSection />
-                        </div>
-                    </div>
-                    </div>}
+                <div className={`absolute inset-0 ${isMobile ? "" : "bg-gradient-to-b from-transparent to-color"}`}></div>
 
+                <div className="flex-col text-white relative z-20">
+                    <Script
+                        src="https://s3-us-west-2.amazonaws.com/bloomerang-public-cdn/lighthousearabworld/.widget-js/113664.js"
+                        strategy="afterInteractive"
+                    />
+
+                    {isMobile ? (
+                        <div className="flex flex-col pt-8">
+                            <div>
+                                <GiftInfo />
+                            </div>
+                            <div className="pb-8">
+                                <DonatingSection />
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="flex-col">
+                            <div className="flex w-full px-12">
+                                <div id='bloomerang-form-113664' className='pt-4 absolute left-40 top-0'></div>
+                                <div className="w-1/2 pt-4 text-black z-20">
+                                    <GiftInfo />
+                                </div>
+                                <div className="w-1/2 pt-28">
+                                    <DonatingSection />
+                                </div>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
-            <div className={`items-center ${isMobile ? "pt-12" : "pt-72 "}`}>
-                    <div className={`${isMobile ? "mb-72" : ""} relative bg-rug h-96 bg-no-repeat bg-cover bg-center`}>
-                <div className="absolute inset-0 bg-gradient-to-b from-transparent to-color"></div>
-                <div className="absolute inset-0 bg-gradient-to-b from-color to-transparent"></div>
+
+            <div className={`items-center ${isMobile ? "pt-12" : "pt-72"}`}>
+                <div className={`${isMobile ? "mb-72" : ""} relative bg-rug h-96 bg-no-repeat bg-cover bg-center`}>
+                    <div className="absolute inset-0 bg-gradient-to-b from-transparent to-color"></div>
+                    <div className="absolute inset-0 bg-gradient-to-b from-color to-transparent"></div>
                 </div>
                 <Contact />
             </div>
